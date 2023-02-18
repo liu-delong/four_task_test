5e08941c4b8773a153d700cea0d05bc5e05cd9f6
liudelong <1589075757@qq.com>
2023-02-18 15:10:04 +0800

    取消main.cpp中宏定义的注释，不影响fmt_test_pro,这些宏定义只是没有传参时的默认参数。fmt_test_pro都是传参了的。

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@
 #include<string>
 using namespace std;
 using namespace std::chrono;
-// #ifndef matrix_size
-// #define matrix_size 1000
-// #endif
-// #ifndef mul_time_1
-// #define mul_time_1 20
-// #endif
-// #ifndef mul_time_2
-// #define mul_time_2 10
-// #endif
+#ifndef matrix_size
+#define matrix_size 1000
+#endif
+#ifndef mul_time_1
+#define mul_time_1 20
+#endif
+#ifndef mul_time_2
+#define mul_time_2 10
+#endif
 
 double matrixA1[matrix_size][matrix_size];
 double matrixB1[matrix_size][matrix_size];
